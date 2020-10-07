5932fa6def11bdc0ca48336f7656622c18696213
pitelinalexey <pitelinalexey@mail.ru>
2020-10-07 22:29:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args){
+        Double doubleMax = Double.MAX_VALUE;
+        Double doubleMin = Double.MIN_VALUE;
+        System.out.println(doubleMax);
+        System.out.println(doubleMin);
+        //===============
+        Integer integerMax = Integer.MAX_VALUE;
+        Integer integerMin = Integer.MIN_VALUE;
+        System.out.println(integerMax);
+        System.out.println(integerMin);
+        //===============
+        Byte byteMax = Byte.MAX_VALUE;
+        Byte byteMin = Byte.MIN_VALUE;
+        System.out.println(byteMax);
+        System.out.println(byteMin);
+        //================
+        Float floatMin = Float.MIN_VALUE;
+        Float floatMax = Float.MAX_VALUE;
+        System.out.println(floatMax);
+        System.out.println(floatMin);
+        //==============
+        Short shortMax = Short.MAX_VALUE;
+        Short shortMin = Short.MIN_VALUE;
+        System.out.println(shortMax);
+        System.out.println(shortMin);
+        //================
+        Long longMax = Long.MAX_VALUE;
+        Long longMin = Long.MIN_VALUE;
+        System.out.println(longMax);
+        System.out.println(longMin);
+
+
+    }
+}
